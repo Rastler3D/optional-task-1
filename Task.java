5199fdf3ab250e7b6eb991fe08af9cec0adff873
Rastler3D <91353346+Rastler3D@users.noreply.github.com>
2024-09-11 05:02:42 +0400

    Add files via upload

---
+++ Task.java
@@ -0,0 +1,15 @@
+//\u000A/*
+    // #if false
+            console.log("Itransition");
+            return;
+    // #endif
+    System.Console.WriteLine("Itransition");
+//*/
+
+//\u000Apublic class Task
+//\u000A{
+//\u000A    public static void main(String[] args)
+//\u000A    {
+//\u000A        System.out.println("Itransition");
+//\u000A    }
+//\u000A}
